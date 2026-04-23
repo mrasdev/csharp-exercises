c432a70676cd34e2adf5d9a170867e77524b6523
Michael Rastetter <github2@michaelrastetter.de>
2026-04-23 08:35:38 +0200

    Add SimpleCalculator.

---
+++ 05_SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿// Simple Calculator in C#. Reads two operands and an operator from the user, performs the calculation, and displays the result.
+
+List<string> validOperators = [ "+", "-", "*", "/" ];
+
+Console.Write("Bitte geben Sie den linken Operanden ein: ");
+if (!double.TryParse(Console.ReadLine(), out double leftOperand))
+{
+    Console.WriteLine("Ungültige Eingabe für den linken Operanden.");
+    return;
+}
+Console.Write("\nBitte geben Sie den rechten Operanden ein: ");
+if (!double.TryParse(Console.ReadLine(), out double rightOperand))
+{
+    Console.WriteLine("Ungültige Eingabe für den rechten Operanden.");
+    return;
+}
+Console.Write("\nBitte geben Sie den Operator ein ({0}): ", string.Join(", ", validOperators));
+string myOperator = (Console.ReadLine() ?? String.Empty).Trim();
+if  (!validOperators.Contains(myOperator))
+{
+    Console.WriteLine("Ungültiger Operator");
+    return;
+}
+
+switch (myOperator)
+{
+    case "+":
+        Console.WriteLine($"{leftOperand} + {rightOperand} = {leftOperand + rightOperand}");
+        break;
+    case "-":
+        Console.WriteLine($"{leftOperand} - {rightOperand} = {leftOperand - rightOperand}");
+        break;
+    case "*":
+        Console.WriteLine($"{leftOperand} * {rightOperand} = {leftOperand * rightOperand}");
+        break;
+    case "/":
+        if (rightOperand == 0)
+        {
+            Console.WriteLine("Division durch Null ist nicht erlaubt.");
+            return;
+        }
+        Console.WriteLine($"{leftOperand} / {rightOperand} = {leftOperand / rightOperand}");
+        break;
+}
