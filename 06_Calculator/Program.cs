96bd0aa17617c0c40297e1fec8e157a2d0693bbe
Michael Rastetter <github2@michaelrastetter.de>
2026-04-24 13:18:42 +0200

    Add project 06_Caclulator

---
+++ 06_Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿string newCalculation = "j";
+do
+{
+    Console.WriteLine("Geben Sie den ersten Operanden ein:");
+    if (!double.TryParse(Console.ReadLine(), out double ersterOperand))
+    {
+        Console.WriteLine("Ungültige Eingabe für den ersten Operanden.");
+        continue;
+    }
+    Console.WriteLine("Geben Sie den zweiten Operanden ein:");
+    if (!double.TryParse(Console.ReadLine(), out double zweiterOperand))
+    {
+        Console.WriteLine("Ungültige Eingabe für den zweiten Operanden.");
+        continue;
+    }
+    Console.WriteLine("Geben Sie den Operator (+, -, *, /) ein:");
+    string operatorInput = Console.ReadLine() ?? string.Empty;
+    if (operatorInput == "/" && zweiterOperand == 0)
+    {
+        Console.WriteLine("Division durch Null ist nicht erlaubt.");
+        continue;
+    }
+    switch(operatorInput)
+    {
+        case "+":
+            Console.WriteLine($"{ersterOperand} + {zweiterOperand} = {ersterOperand + zweiterOperand}");
+            break;
+        case "-":
+            Console.WriteLine($"{ersterOperand} - {zweiterOperand} = {ersterOperand - zweiterOperand}");
+            break;
+        case "*":
+            Console.WriteLine($"{ersterOperand} * {zweiterOperand} = {ersterOperand * zweiterOperand}");
+            break;
+        case "/":
+            Console.WriteLine($"{ersterOperand} / {zweiterOperand} = {ersterOperand / zweiterOperand}");
+            break;
+        default:
+            Console.WriteLine("Ungültiger Operator.");
+            continue;
+    }
+    Console.WriteLine("Nochmals eingeben? (j/n)");
+    newCalculation = Console.ReadLine() ?? string.Empty;
+} while (newCalculation.Trim().Equals("j", StringComparison.OrdinalIgnoreCase));
