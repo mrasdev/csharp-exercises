3447ef9eefe4c8b6fe72a703a6f9c60c36149827
Michael Rastetter <github2@michaelrastetter.de>
2026-04-28 10:40:30 +0200

    Restructure directories

---
+++ 12_Hafner_Array/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_Hafner_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
