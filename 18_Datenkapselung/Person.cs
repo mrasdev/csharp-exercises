2d0dd453fadfba8307d7695ac35424b1a90f2624
Michael Rastetter <github2@michaelrastetter.de>
2026-05-06 12:42:23 +0200

    Add class Person

---
+++ 18_Datenkapselung/Person.cs
@@ -0,0 +1,55 @@
+﻿namespace _18_Datenkapselung;
+
+internal class Person
+{
+    string _vorname;
+    string _nachname;
+
+    public Person()
+    {
+    }
+
+    public Person(string vorname, string nachname)
+    {
+        SetzeVorname(vorname);
+        SetzeNachname(nachname);
+    }
+
+    public void SetzeNachname(string nachname = "")
+    {
+        if (nachname.Length > 0)
+        {
+            _nachname = nachname;
+            return;
+        }
+        Console.Write("Bitte Nachnamen eingeben: ");
+        _nachname = Console.ReadLine() ?? string.Empty;
+    }
+
+    public void SetzeVorname(string vorname = "")
+    {
+        if (vorname.Length > 0)
+        {
+            _vorname = vorname;
+            return;
+        }
+        Console.Write("Bitte Vornamen eingeben: ");
+        _vorname = Console.ReadLine() ?? string.Empty;
+    }
+
+    public void SagHallo()
+    {
+        if (_vorname.Length == 0 || _nachname.Length == 0)
+        {
+            Console.WriteLine("FEHLER: Vor- bzw. Nachname ist nicht definiert.");
+            return;
+        }
+        Console.WriteLine($"Hallo, ich heiße {_vorname} {_nachname}.");
+    }
+
+    public void LoescheNamen()
+    {
+        _vorname = "";
+        _nachname = "";
+    }
+}
---
+++ 18_Datenkapselung/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _18_Datenkapselung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestePerson();
+        }
+
+        private static void TestePerson()
+        {
+            Person person = new Person();  // Rufe Default Konstruktor ohne Inhalt auf
+            person.SetzeVorname();  // Frage Name vom Anwender ab und speichere ihn im Objekt
+            person.SetzeNachname();  // Frage Name vom Anwender ab und speichere ihn im Objekt
+            person.SagHallo();
+            person = new Person(vorname: "Michael", nachname: "Meier");  // Neues Objekt mit definierten Werten
+            person.SagHallo();
+            person.LoescheNamen();
+            person.SagHallo();
+            person.SetzeVorname("Martin");
+            person.SetzeNachname("Müller");
+            person.SagHallo();
+        }
+    }
+}
