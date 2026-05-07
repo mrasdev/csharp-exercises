0e4d8f5221e0a03f52b33eef5e1b146fb1d0befe
Michael Rastetter <github2@michaelrastetter.de>
2026-05-07 13:36:49 +0200

    Add TemperaturStatistik project

---
+++ 19_TemperaturStatistik/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+namespace _19_TemperaturStatistik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            TemperaturStatistik temperaturStatistik = new TemperaturStatistik();
+            //temperaturStatistik.NeueMessungErfassen(5);
+            //temperaturStatistik.NeueMessungErfassen(6);
+            //temperaturStatistik.NeueMessungErfassen(7);
+            for (int i = 0; i < 100; i++)
+            {
+                temperaturStatistik.NeueMessungErfassen(rnd.NextDouble() * 100.0);
+            }
+            // ConsoleWriteLineAusprobiererle(temperaturStatistik);
+            Console.WriteLine($"Durschnitt = {temperaturStatistik.Durchschnitt():f2} °C");  // = 6
+            Console.WriteLine($"Maximum = {temperaturStatistik.Maximalwert():f2} °C");  // = 7
+        }
+
+        private static void ConsoleWriteLineAusprobiererle(TemperaturStatistik temperaturStatistik)
+        {
+            Console.WriteLine("Durchschnitt = " + temperaturStatistik.Durchschnitt() + " und das Maximum ");
+            string vorname = "Pierre";
+            string nachname = "Anders";
+            double zahl = 23.6546786654687654654;
+            Console.WriteLine("Ich heiße {0} und mein kompletter Name ist {0} {1} und bin AE.", vorname, nachname);
+            Console.WriteLine($"Ich heiße {vorname} und mein kompletter Name ist {vorname} {nachname} und bin AE.");
+            Console.WriteLine($"Zahl = {zahl:f3}");
+            double durchschnitt = temperaturStatistik.Durchschnitt();
+            Console.WriteLine($"Durschnitt = {durchschnitt:f3}");
+        }
+    }
+}
---
+++ 19_TemperaturStatistik/TemperaturStatistik.cs
@@ -0,0 +1,22 @@
+﻿namespace _19_TemperaturStatistik
+{
+    internal class TemperaturStatistik
+    {
+        private List<double> _messwerte = new List<double>();
+
+        public void NeueMessungErfassen(double wert)
+        {
+            _messwerte.Add(wert);
+        }
+
+        public double Durchschnitt()
+        { 
+            return _messwerte.Average();
+        }
+
+        public double Maximalwert()
+        {
+            return _messwerte.Max();
+        }
+    }
+}
